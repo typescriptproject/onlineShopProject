5f03b8cb06a54a25a0ebc4b75dd635f4cea995fc
BONGJIN CHO <bjchomr@naver.com>
2023-02-01 18:12:16 +0900

    초기설정

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = require('./src/routes');
+const app = express();
+
+
+app.use('/api', router);
+
+
+app.listen(process.env.PORT, () => {
+	console.log(process.env.PORT, '포트로 서버가 열렸어요!');
+});
+  
+module.exports = app;
